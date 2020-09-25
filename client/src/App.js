cae65467279a1dadf878b27594817bb8fcc0927c
asonbede <asonbede@gmail.com>
2020-09-25 23:34:27 +0100

    third commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,29 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 const App = () => {
   const [user, setUser] = useState(null);
+  const [searchStr, setsearchStr] = useState("asonbede");
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   axios
+  //     .get("/api")
+  //     .then((response) => {
+  //       console.log(response.data, "clienttttt");
+  //       setUser(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.log("error occured");
+  //     });
+  // }, []);
+
+  const handleChange = (event) => {
+    setsearchStr(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
     axios
-      .get("/api")
+      .get(`/api?user=${searchStr}`)
       .then((response) => {
         console.log(response.data, "clienttttt");
         setUser(response.data);
@@ -14,11 +33,15 @@ const App = () => {
       .catch((error) => {
         console.log("error occured");
       });
-  }, []);
+  };
 
   return (
     user && (
       <div>
+        <form onSubmit={handleSubmit}>
+          <input value={searchStr} onChange={handleChange} />
+          <button type="submit">submit </button>
+        </form>
         <p>{JSON.stringify(user)}</p>
       </div>
     )
